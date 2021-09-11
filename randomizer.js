0c20115b6c94258293d2d8ba9bcc37f8955298ee
Gibson Ainge <gainge@gmail.com>
2021-09-11 04:08:44 -0600

    implement dfs path building

--- randomizer.js
+++ randomizer.js
@@ -31,59 +31,67 @@ const settings = {
 	speedrunCodes: true,
 }
 
+const TTRC_EXCLUSIONS = [
+	[45, 28, 44], // doc
+	[30, 33, 40], // mario
+	[49, 32, 46], // luigi
+	[42, 35, 32], // bowser
+	[48, 37, 41], // peach
+	[31, 48, 28], // yoshi
+	[47, 38, 26], // dk
+	[46, 31, 42], // falcon
+	[27, 30, 34], // ganon
+	[41, 44, 29], // falco
+	[34, 49, 31], // fox
+	[29, 45, 25], // ness
+	[33, 41, 43], // ICs
+	[43, 42, 36], // kirby
+	[35, 43, 37], // samus
+	[37, 29, 27], // sheik
+	[28, 26, 30], // link
+	[40, 25, 35], // yl
+	[32, 36, 49], // pichu
+	[39, 47, 45], // pika
+	[38, 34, 48], // puff
+	[36, 39, 38], // m2
+	[26, 27, 39], // gnw
+	[44, 40, 33], // marth
+	[25, 46, 47], // roy
+]
+const numChars = TTRC_EXCLUSIONS.length
 
+
+// optimized fisher yates pog https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+function shuffleArray(array) {
+	for (let i = array.length - 1; i > 0; i--) {
+			const j = Math.floor(Math.random() * (i + 1));
+			[array[i], array[j]] = [array[j], array[i]];
+	}
+}
 
 function buildGraph() {
-	const TTRC_EXCLUSIONS = [
-		[45, 28, 44], // doc
-		[30, 33, 40], // mario
-		[49, 32, 46], // luigi
-		[42, 35, 32], // bowser
-		[48, 37, 41], // peach
-		[31, 48, 28], // yoshi
-		[47, 38, 26], // dk
-		[46, 31, 42], // falcon
-		[27, 30, 34], // ganon
-		[41, 44, 29], // falco
-		[34, 49, 31], // fox
-		[29, 45, 25], // ness
-		[33, 41, 43], // ICs
-		[43, 42, 36], // kirby
-		[35, 43, 37], // samus
-		[37, 29, 27], // sheik
-		[28, 26, 30], // link
-		[40, 25, 35], // yl
-		[32, 36, 49], // pichu
-		[39, 47, 45], // pika
-		[38, 34, 48], // puff
-		[36, 39, 38], // m2
-		[26, 27, 39], // gnw
-		[44, 40, 33], // marth
-		[25, 46, 47], // roy
-	]
-	const numChars = TTRC_EXCLUSIONS.length
+	// create the adjacency list
+	const graph = []
+	for (let i = 0; i < numChars * 2; i++) {
+		let row = [];
 
-	const graph = Array.from(Array(numChars * 2), _ => Array(numChars * 2).fill(1));
-
-	// Remove TTRC Exclusions
-	TTRC_EXCLUSIONS.forEach((stages, char) => {
-		stages.forEach(stageIndex => {
-			graph[char][stageIndex] = 0; // Remove this link
-		})
-	});
-
-	// Remove character to character connections
-	for (let i = 0; i < numChars; i++) {
-		for (let j = 0; j < numChars; j++) {
-			graph[i][j] = 0;
+		if (i < numChars) {
+			// Character mode, assign only to stages
+			for (let j = numChars; j < numChars * 2; j++) {
+				// Exlude stages that are in the TTRC exclusions
+				if (!TTRC_EXCLUSIONS[i].includes(j)) {
+					row.push(j);
+				}
+			}
+		} else {
+			// Stage mode, assign to all chars and no stages
+			for (let j = 0; j < numChars; j++) {
+				row.push(j);
+			}
 		}
-	}
 
-	// Remove stage to stage connections
-	for (let i = 0; i < numChars; i++) {
-		for (let j = 0; j < numChars; j++) {
-			graph[i + numChars][j + numChars] = 0;
-		}
+		// Add this row to our adjacency list
+		graph.push(row)
 	}
 
 	// Making that adjacency matrix, dawwwwg
@@ -106,55 +114,60 @@ function initPathMap(graph) {
 	return sets
 }
 
+function randomizeGraph(graph) {
+	for (let i = 0; i < graph.length / 2; i++) {
+		shuffleArray(graph[i]);
+	}
+}
+
+
+
+function buildHamiltonian(graph, visited, parent, path) {
+	// Check base case at some point?
+	if (path.length === 50) return true; // We've done everything we can, this is the end!
+
+	// Otherwise, mark that we were here
+	visited.push(parent);
+
+	neighbors = graph[parent];
+
+	// Iterate over parent's neighbors, attempting to extend path
+	for (let i = 0; i < neighbors.length; i++) {
+		// Attempt to branch off here if not visited
+		if (visited.includes(neighbors[i])) continue;
+
+		// Append to path and recurse!
+		path.push(neighbors[i]);
+
+		if (buildHamiltonian(graph, visited, neighbors[i], path)) {
+			// This is a complete path! sick!
+			return true;
+		} else {
+			// Something failed down the line, we need to try again
+			path.pop();
+		}
+	}
+	
+	// We didn't find anything successful, return false
+	visited.pop(); // Should remove ourselves I think
+	return false;
+}
+
 // Hmmmm ok let's try to code a hamiltonian path thing
 // Gonna try to make the hackiest solution of all time
 function buildPath() {
 	const graph = buildGraph(); // Adjacency matrix
+	randomizeGraph(graph);
 
-	// For every vertex v, pathMap[v] contains a set(list) of all the paths that end in v
-	let pathMap = initPathMap(graph);
+	let visited = [];
+	let parent = 0;
+	let path = [parent];
 
-	// Now we do the alg thing ye?
-	// Build until the paths are of full length?
-	for (let i = 0; i < graph.length - 1; i++) { // might be off by one here lol, since we already start with size 1
-		console.log("pass " + i);
-		let newPathMap = [];
+	buildHamiltonian(graph, visited, parent, path)
 
-		// For every vertex
-		for (let v = 0; v < graph.length; v++) {
-			console.log("vertex " + v);
-			newPathMap.push([]);
-
-			// Iterate over EVERY EXISTING SUBSET OF PATHS and check for connectivity
-			pathMap.forEach((paths, w) => {
-				// Don't check our own paths, though this would get excluded below anyway
-				if (w !== v) {
-					// For every path ending in vertex w
-					paths.forEach(path => {
-						// If we connect to w and are not already in the path
-						if (graph[v][w] && !path.includes(v)) {
-							// console.log("connecting path " + path +" through vertex " + w);
-							// This is a new path for us :eyes:
-							let newPath = [v, ...path];
-
-							// console.log(newPath);
-							// Add to the map under our name :)
-							newPathMap[v].push(newPath);
-						}
-					});
-				}
-			});
-		}
-
-		// Now we should have built a whole new generation of path subsets
-		// let's re-assign and continue with our lives!
-		pathMap = newPathMap;
-		console.log(pathMap);
-	}
-
-	// Now we can return one of our paths?
-	// idk let's just print this is LOL
-	console.log(pathMap);
+	console.log(path);
+	
+	return;
 }
 
 
